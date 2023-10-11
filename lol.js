ca74653843633fc95f1a529fd0dd45c08b418a61
shishir <kunjgupta1265@gmail.com>
2023-10-11 11:52:32 +0530

    l

--- lol.js
+++ lol.js
@@ -1,14 +1,21 @@
 const nav = document.querySelector('.nav-ul');
-
+const click = document.querySelector('.menu-bar-button');
 const navbutton = document.querySelector('.menu-bar-button');
 
 navbutton.addEventListener('click', () => {
     const visibility = nav.getAttribute('data-visible');
-    console.log('click');
     if (visibility === "false") {
         nav.setAttribute('data-visible', true);
     }
     else {
         nav.setAttribute('data-visible', false)
     }
+    const animatebutton = click.getAttribute('data-click');
+    if (animatebutton === "false") {
+        click.setAttribute('data-click', true);
+    }
+    else {
+        click.setAttribute('data-click', false);
+    }
 })
+
